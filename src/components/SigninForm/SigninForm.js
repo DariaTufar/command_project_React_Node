ff3a3f944af481f2b0d830754aef743fbf4fe80b
Dariia Shenkevych <65854631+DariaTufar@users.noreply.github.com>
2023-04-05 14:41:32 +0300

    Revert "Update SigninForm.js"

--- src/components/SigninForm/SigninForm.js
+++ src/components/SigninForm/SigninForm.js
@@ -6,7 +6,6 @@ import { CgLock } from 'react-icons/cg';
 import { Icon } from '../Icon/Icon';
 
 import { ERROR_COLOR, SIGNIN_DEFAULT_ERROR_STATE } from '../../constants/form';
-import { useSignIn } from 'api/hooks';
 
 export const SigninForm = () => {
   const [signInData, setSignInData] = useState({
@@ -16,31 +15,32 @@ export const SigninForm = () => {
 
   const [signInErrors, setSignInErrors] = useState(SIGNIN_DEFAULT_ERROR_STATE);
 
-  const { email, password } = signInData;
+  const {
+    email,
+    password,
+  } = signInData;
 
-  const { emailError, passwordError } = signInErrors;
-
-  const { mutate } = useSignIn();
+  const {
+    emailError,
+    passwordError,
+  } = signInErrors
 
   const handleSignIn = () => {
-    const errors = validateFormFields(signInData);
+    const errors = validateFormFields(signInData)
+
 
     if (!isNil(errors)) {
       return setSignInErrors(errors);
     }
 
     setSignInErrors(SIGNIN_DEFAULT_ERROR_STATE);
-    mutate({ email, password });
-  };
+  }
 
   return (
     <div className="form-element signin-form">
       <div className="inn">
-        <form
-          className="form"
-          autoComplete="off"
-          onSubmit={e => e.preventDefault()}
-        >
+
+        <form className="form" autoComplete="off" onSubmit={(e) => e.preventDefault()}>
           <h3>Sign In</h3>
           <div className="field">
             <label htmlFor="email">
@@ -53,9 +53,7 @@ export const SigninForm = () => {
                 value={email}
                 placeholder="Email"
                 autoComplete="new-email"
-                onChange={e =>
-                  setSignInData({ ...signInData, email: e.target.value })
-                }
+                onChange={(e) => setSignInData({...signInData, email: e.target.value})}
               />
             </label>
             {emailError && <div className="field-error-text">{emailError}</div>}
@@ -71,20 +69,15 @@ export const SigninForm = () => {
                 value={password}
                 name="password"
                 placeholder="Password"
-                onChange={e =>
-                  setSignInData({ ...signInData, password: e.target.value })
-                }
+                onChange={(e) => setSignInData({...signInData, password: e.target.value})}
               />
             </label>
-            {passwordError && (
-              <div className="field-error-text">{passwordError}</div>
-            )}
+            {passwordError && <div className="field-error-text">{passwordError}</div>}
           </div>
 
-          <button className="submit-button" onClick={() => handleSignIn()}>
-            Sign In
-          </button>
+          <button className="submit-button" onClick={() => handleSignIn()}>Sign In</button>
         </form>
+
       </div>
     </div>
   );
